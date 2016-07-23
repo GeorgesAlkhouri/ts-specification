1c697529586887bb2e2df08ab1b2f759018da004
Georges Alkhouri <georges.alkhouri@gmail.com>
2016-07-23 18:00:48 +0200

    [FIX] Added missing methods to interface

--- src/ISpecification.ts
+++ src/ISpecification.ts
@@ -1,7 +1,9 @@
 interface ISpecification<T> {
         isSatisfiedBy(candidate: T): boolean
         and(other: ISpecification<T>): ISpecification<T>
+        andNot(other: ISpecification<T>): ISpecification<T>
         or(other: ISpecification<T>): ISpecification<T>
+        orNot(other: ISpecification<T>): ISpecification<T>
         not(): ISpecification<T>
 }
 
